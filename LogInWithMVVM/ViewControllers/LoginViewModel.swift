4eea1fe5ac3de4a5806dfcddd05248324ca53404
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-05-18 23:12:54 +0500

    LoginViewModel logic updated.

--- LogInWithMVVM/ViewControllers/LoginViewModel.swift
+++ LogInWithMVVM/ViewControllers/LoginViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol LoginViewModelProtocol {
+    var personName: String {get}
+    var personSurname: String {get}
+    init(person: Person)
+}
+
+class LoginViewModel: LoginViewModelProtocol {
+    
+    var personName: String {
+        person.name
+    }
+    
+    var personSurname: String {
+        person.surname
+    }
+    
+    private let person: Person
+    
+    required init(person: Person) {
+        self.person = person
+    }
+}
